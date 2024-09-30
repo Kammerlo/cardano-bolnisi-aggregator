3c2c75ac154e7490bda3fbbf0961ea27e1093602
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-09-30 14:40:23 +0200

    fix: added filtering for duplicates in data

--- src/main/java/org/cardanofoundation/bolnisiaggregator/processor/BolnisiProcessor.java
+++ src/main/java/org/cardanofoundation/bolnisiaggregator/processor/BolnisiProcessor.java
@@ -8,7 +8,6 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
